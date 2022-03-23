74c81a136abdd54a4013fa4ab97017d141dfe193
Joseph <entewshi@gmail.com>
2022-03-23 03:39:37 +0100

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
+      <h1 className="text-9xl font-black text-white text-center">
+        <span className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+          this is a test app
+        </span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
